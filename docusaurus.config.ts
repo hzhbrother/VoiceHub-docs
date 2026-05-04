6d9719fb26a69e39d65d13b8e49b34d2d33af8b5
hzh哥 <hzh15014080363@gmail.com>
2026-05-04 13:13:43 +0800

    添加文档版本号1.0.6到底部栏

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} 华青之声点歌台. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} 华青之声点歌台. Built with Docusaurus. | 文档版本 1.0.6`,
     },
     prism: {
       theme: prismThemes.github,
